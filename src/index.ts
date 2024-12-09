a2dce009103cebfdd98612fbdac5a9d117a30e5d
syaiful <143513644+Syaiful313@users.noreply.github.com>
2024-12-09 17:41:24 +0700

    feat: register (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
 import express, { Request, Response, NextFunction } from "express";
 import { PORT } from "./config";
-import sampleRouter from "./routes/sample.router"
-import cors from "cors"
+import sampleRouter from "./routes/sample.router";
+import authRouter from "./routes/auth.router";
+import cors from "cors";
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use("/samples", sampleRouter)
+app.use("/samples", sampleRouter);
+app.use("/auth", authRouter);
 
-
-// middleware error 
-app.use((err:Error , req: Request , res: Response ,next: NextFunction) => {
-    res.status(400).send(err.message)
-})
+// middleware error
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(400).send(err.message);
+});
 
 app.listen(PORT, () => {
   console.log(`server running on PORT : ${PORT}`);
