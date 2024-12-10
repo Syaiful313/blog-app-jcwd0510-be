0e31b014a2ceebc2117403e73b39426469363688
Syaiful313 <mthitz313@gmail.com>
2024-12-10 09:41:46 +0700

    feat: forgot password

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { loginController, registerController } from "../controllers/auth.controller";
-import { validateLogin, validateRegister } from "../validators/auth.validator";
+import { forgotPasswordController, loginController, registerController } from "../controllers/auth.controller";
+import { validateForgotPassword, validateLogin, validateRegister } from "../validators/auth.validator";
 
 const router = Router();
 
 router.post("/register",validateRegister , registerController);
 router.post("/login",validateLogin , loginController);
+router.post("/forgot-password",validateForgotPassword , forgotPasswordController);
 
 export default router;
